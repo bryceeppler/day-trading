d63ad0defa54bfd4ed2710a46c92542f52ca9bb5
Bryce Eppler <eppler97@gmail.com>
2024-01-26 13:45:53 -0800

    accessing db uri from env variables in each service

--- ms_market_data/index.js
+++ ms_market_data/index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 
+// mongo db uri for this microservice's database
+// const mongoUri = process.env.MONGO_URI;
+
 app.get("/", (req, res) => {
   res.send("This is the market data microservice");
 });

c7315a2ff52d9063529442627702157d464eeb57
Matthew Buie <buiesoftwaredev@gmail.com>
2024-02-17 08:12:28 -0800

    Add auth to routes

--- ms_order_creation/routes/orders.routes.js
+++ ms_order_creation/routes/orders.routes.js
@@ -4,6 +4,6 @@ const { authenticateToken } = require('./middleware/authentication');
 const validation = require('./validations/orders.validation')
 
 module.exports = (router) => {
-  router.route('/placeStockOrder').post([/*authenticateToken,*/ validation.placeStockOrder], orders.placeStockOrder);
-  router.route('/cancelStockTransaction').post([/*authenticateToken,*/ validation.cancelStockTransaction], orders.cancelStockTransaction);
+  router.route('/placeStockOrder').post([authenticateToken, validation.placeStockOrder], orders.placeStockOrder);
+  router.route('/cancelStockTransaction').post([authenticateToken, validation.cancelStockTransaction], orders.cancelStockTransaction);
 };

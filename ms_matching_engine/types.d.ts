5a8be4d3c7dc4ab41d0af0363c80cbb01d88c14a
Bryce Eppler <eppler97@gmail.com>
2024-02-18 06:11:08 -0800

    type defs

--- ms_matching_engine/types.d.ts
+++ ms_matching_engine/types.d.ts
@@ -1,25 +1,45 @@
+import { Order } from "./types.d";
 export enum OrderType {
-    LIMIT = "LIMIT",
-    MARKET = "MARKET"
-  }
-  
+  LIMIT = "LIMIT",
+  MARKET = "MARKET",
+}
+
+export enum OrderStatus {
+  IN_PROGRESS = "IN_PROGRESS",
+  CANCELED = "CANCELED",
+  COMPLETED = "COMPLETED",
+}
+
+export interface StockTransaction {
+  stock_id: string;
+  wallet_tx_id: string;
+  portfolio_id: string;
+  order_status: ORDER_STATUS;
+  is_buy: boolean;
+  order_type: ORDER_TYPE;
+  stock_price: number;
+  quantity: number;
+  time_stamp: Date;
+  is_deleted: boolean;
+}
+
 export interface Order {
-    _id?: number;
-    stock_id: string;
-    stock_price: number;
-    quantity: number;
-    is_buy: boolean;
-    type: OrderType;
-  }
-  
-  export interface MatchedOrder {
-    buyOrder: Order;
-    sellOrder: Order;
-    quantity: number;
-    matchPrice: number;
-    timestamp: Date;
-  }
-  
-  export interface StockTransactionModel {
-    find(query: object): Promise<Order[]>;
-  }
+  _id?: number;
+  stock_id: string;
+  stock_price: number;
+  quantity: number;
+  is_buy: boolean;
+  type: OrderType;
+}
+
+export interface MatchedOrder {
+  buyOrder: Order;
+  sellOrder: Order;
+  quantity: number;
+  matchPrice: number;
+  timestamp: Date;
+}
+
+export interface StockTransactionModel {
+  find(query: object): Promise<Order[]>;
+}

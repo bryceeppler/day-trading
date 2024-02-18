7aaaef884ff07eced0a075994fe8dbc84d99b809
Bryce Eppler <eppler97@gmail.com>
2024-02-18 07:06:12 -0800

    Converting jest tests to typescript

---
+++ ms_matching_engine/services/orderbook.test.ts
@@ -0,0 +1,199 @@
+import OrderBook from './orderbook';
+import {
+  Order,
+  MatchedOrder,
+  IOrderBook
+} from "../types";
+
+describe("OrderBook", () => {
+  let orderbook;
+
+  beforeEach(() => {
+    orderbook = new OrderBook();
+  });
+
+  it("matches simple full buy order", () => {
+    orderbook.sellOrders = [
+      { _id: 1, is_buy: false, stock_id: "1", stock_price: 10, quantity: 5, type: "LIMIT" },
+    ];
+    const order = {
+      _id:2,
+      is_buy: true,
+      stock_id: "1",
+      stock_price: 10,
+      quantity: 5,
+      type: "LIMIT",
+    };
+    const [ matched, remainingQuantity ] = orderbook.matchOrder(order);
+    
+
+    expect(orderbook.matchedOrders.length).toBe(1);
+    expect(matched.length).toBe(1);
+    expect(remainingQuantity).toBe(0);
+  });
+
+  // it("matches partial buy order with remaining quantity", () => {
+  //   orderbook.sellOrders = [
+  //     { _id: 1, stock_id: "1", stock_price: 10, quantity: 3 },
+  //   ];
+  //   const order = {
+  //     is_buy: true,
+  //     stock_id: "1",
+  //     stock_price: 10,
+  //     quantity: 5,
+  //   };
+  //   const { matched, remainingQuantity } = orderbook.matchOrder(order);
+
+  //   expect(orderbook.matchedOrders.length).toBe(1);
+  //   expect(matched.length).toBe(1);
+  //   expect(remainingQuantity).toBe(2);
+  // });
+
+  // it("matches partial sell order with remaining quantity", () => {
+  //   orderbook.buyOrders = [
+  //     { _id: 1, stock_id: "1", stock_price: 10, quantity: 3 },
+  //   ];
+  //   const order = {
+  //     is_buy: false,
+  //     stock_id: "1",
+  //     stock_price: 10,
+  //     quantity: 5,
+  //   };
+  //   const { matched, remainingQuantity } = orderbook.matchOrder(order);
+
+  //   expect(orderbook.matchedOrders.length).toBe(1);
+  //   expect(matched.length).toBe(1);
+  //   expect(remainingQuantity).toBe(2);
+  // });
+
+  // it("matches multiple full buy orders", () => {
+  //   orderbook.sellOrders = [
+  //     { _id: 1, stock_id: "1", stock_price: 10, quantity: 5 },
+  //     { _id: 2, stock_id: "1", stock_price: 10, quantity: 5 },
+  //   ];
+  //   const order = {
+  //     is_buy: true,
+  //     stock_id: "1",
+  //     stock_price: 10,
+  //     quantity: 10,
+  //   };
+  //   const { matched, remainingQuantity } = orderbook.matchOrder(order);
+
+  //   expect(orderbook.matchedOrders.length).toBe(2);
+  //   expect(matched.length).toBe(2);
+  //   expect(remainingQuantity).toBe(0);
+  // });
+
+  // it("matches multiple full sell orders", () => {
+  //   orderbook.buyOrders = [
+  //     { _id: 1, stock_id: "1", stock_price: 12, quantity: 5 },
+  //     { _id: 2, stock_id: "1", stock_price: 12, quantity: 5 },
+  //   ];
+  //   const order = {
+  //     is_buy: false,
+  //     stock_id: "1",
+  //     stock_price: 12,
+  //     quantity: 10,
+  //   };
+  //   const { matched, remainingQuantity } = orderbook.matchOrder(order);
+
+  //   expect(orderbook.matchedOrders.length).toBe(2);
+  //   expect(matched.length).toBe(2);
+  //   expect(remainingQuantity).toBe(0);
+  // });
+
+  // it("matches multiple partial sell orders with remaining quantity", () => {
+  //   orderbook.buyOrders = [
+  //     { _id: 1, stock_id: "1", stock_price: 15, quantity: 3 },
+  //     { _id: 2, stock_id: "1", stock_price: 15, quantity: 2 },
+  //   ];
+  //   const order = {
+  //     is_buy: false,
+  //     stock_id: "1",
+  //     stock_price: 15,
+  //     quantity: 10,
+  //   };
+  //   const { matched, remainingQuantity } = orderbook.matchOrder(order);
+
+  //   expect(orderbook.matchedOrders.length).toBe(2);
+  //   expect(matched.length).toBe(2);
+  //   expect(remainingQuantity).toBe(5);
+  // });
+
+  // it("matches multiple partial buy orders with remaining quantity", () => {
+  //   orderbook.sellOrders = [
+  //     { _id: 1, stock_id: "1", stock_price: 8, quantity: 4 },
+  //     { _id: 2, stock_id: "1", stock_price: 8, quantity: 1 },
+  //   ];
+  //   const order = {
+  //     is_buy: true,
+  //     stock_id: "1",
+  //     stock_price: 8,
+  //     quantity: 10,
+  //   };
+  //   const { matched, remainingQuantity } = orderbook.matchOrder(order);
+
+  //   expect(orderbook.matchedOrders.length).toBe(2);
+  //   expect(matched.length).toBe(2);
+  //   expect(remainingQuantity).toBe(5);
+  // });
+
+  // it("matches multiple full buy and sell orders", () => {
+  //   orderbook.sellOrders = [
+  //     { _id: 1, stock_id: "1", stock_price: 20, quantity: 5 },
+  //     { _id: 2, stock_id: "1", stock_price: 20, quantity: 5 },
+  //   ];
+  //   orderbook.buyOrders = [
+  //     { _id: 3, stock_id: "1", stock_price: 20, quantity: 5 },
+  //     { _id: 4, stock_id: "1", stock_price: 20, quantity: 5 },
+  //   ];
+  //   const sellOrder = {
+  //     is_buy: false,
+  //     stock_id: "1",
+  //     stock_price: 20,
+  //     quantity: 10,
+  //   };
+  //   const buyOrder = {
+  //     is_buy: true,
+  //     stock_id: "1",
+  //     stock_price: 20,
+  //     quantity: 10,
+  //   };
+  //   const sellMatch = orderbook.matchOrder(sellOrder);
+  //   const buyMatch = orderbook.matchOrder(buyOrder);
+
+  //   expect(orderbook.matchedOrders.length).toBe(2);
+  //   expect(sellMatch.matched.length + buyMatch.matched.length).toBe(2);
+  //   expect(sellMatch.remainingQuantity + buyMatch.remainingQuantity).toBe(0);
+  // });
+
+  // it("matches multiple partial buy and sell orders with remaining quantities", () => {
+  //   orderbook.sellOrders = [
+  //     { _id: 1, stock_id: "1", stock_price: 25, quantity: 3 },
+  //     { _id: 2, stock_id: "1", stock_price: 25, quantity: 2 },
+  //   ];
+  //   orderbook.buyOrders = [
+  //     { _id: 3, stock_id: "1", stock_price: 25, quantity: 4 },
+  //     { _id: 4, stock_id: "1", stock_price: 25, quantity: 1 },
+  //   ];
+  //   const sellOrder = {
+  //     is_buy: false,
+  //     stock_id: "1",
+  //     stock_price: 25,
+  //     quantity: 10,
+  //   };
+  //   const buyOrder = {
+  //     is_buy: true,
+  //     stock_id: "1",
+  //     stock_price: 25,
+  //     quantity: 10,
+  //   };
+  //   const sellMatch = orderbook.matchOrder(sellOrder);
+  //   const buyMatch = orderbook.matchOrder(buyOrder);
+
+  //   expect(orderbook.matchedOrders.length).toBe(4);
+  //   expect(sellMatch.matched.length + buyMatch.matched.length).toBe(4);
+  //   expect(sellMatch.remainingQuantity).toBe(5);
+  //   expect(buyMatch.remainingQuantity).toBe(5);
+  // });
+});

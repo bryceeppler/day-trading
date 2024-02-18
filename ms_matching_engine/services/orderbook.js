bdbc00ff124e4dc966e82e99f504db56912b42ac
Bryce Eppler <eppler97@gmail.com>
2024-02-17 20:19:52 -0800

    Adding docstrings

--- ms_matching_engine/services/orderbook.js
+++ ms_matching_engine/services/orderbook.js
@@ -9,6 +9,10 @@ module.exports = class OrderBook {
     this.expiredOrders = [];
     // this.init();
   }
+
+  /**
+   * Match a market order against the orderbook and return the matched orders
+   */
   matchMarketOrder(newOrder) {
     const orderQueue = newOrder.is_buy ? this.sellOrders : this.buyOrders;
     let remainingQty = newOrder.quantity;
@@ -20,11 +24,13 @@ module.exports = class OrderBook {
       remainingQty -= matchedQuantity;
       matchAgainst.quantity -= matchedQuantity;
 
-      this.matchedOrders.push(this.createMatchedOrder(newOrder, matchAgainst, matchedQuantity));
+      this.matchedOrders.push(
+        this.createMatchedOrder(newOrder, matchAgainst, matchedQuantity)
+      );
 
       if (matchAgainst.quantity === 0) {
-        orderQueue.splice(i, 1);
-        i--; 
+        orderQueue.splice(i, 1); // remove fully matched order from the orderbook
+        i--;
       }
     }
 
@@ -51,6 +57,9 @@ module.exports = class OrderBook {
     );
   }
 
+  /**
+   * Only run on init, loads all in progress orders from the database.
+   */
   async loadOrders() {
     let allBuyOrders = await this.stockTransactionModel
       .find({ order_type: "LIMIT", is_buy: true, order_status: "IN_PROGRESS" })
@@ -63,11 +72,17 @@ module.exports = class OrderBook {
     this.sellOrders = allSellOrders;
   }
 
+  /**
+   * Check for expired orders and add them to the expiredOrders array
+   */
   checkForExpiredOrders() {
     console.log("checking for expired orders");
-    return
+    return;
   }
 
+  /**
+   * Given a pair of matched orders, create a new matched order object
+   */
   createMatchedOrder(order, matchAgainst, quantity) {
     return {
       buyOrder: order.is_buy ? order : matchAgainst,
@@ -78,6 +93,9 @@ module.exports = class OrderBook {
     };
   }
 
+  /**
+   * Find all matches for a given order and return the matched orders and the remaining quantity
+   */
   findMatches(order) {
     const matched = [];
     let remainingQty = order.quantity;
@@ -90,36 +108,55 @@ module.exports = class OrderBook {
       remainingQty -= matchedQuantity;
       matchAgainst.quantity -= matchedQuantity;
 
-      matched.push(this.createMatchedOrder(order, matchAgainst, matchedQuantity));
+      matched.push(
+        this.createMatchedOrder(order, matchAgainst, matchedQuantity)
+      );
       if (matchAgainst.quantity === 0) orderQueue.splice(i, 1); // Remove fully matched orders
     });
 
-    return { matched, remainingQty };
+    return matched;
   }
   isMatch(order, matchAgainst) {
-    return order.is_buy ? matchAgainst.stock_price <= order.stock_price : matchAgainst.stock_price >= order.stock_price;
+    return order.is_buy
+      ? matchAgainst.stock_price <= order.stock_price
+      : matchAgainst.stock_price >= order.stock_price;
   }
 
+  /**
+   * Handle remaining quantity of a partially filled order by pushing it into the orderbook
+   */
   handlePartialOrder(order, remainingQty) {
     if (remainingQty > 0 && order.quantity !== remainingQty) {
       this.insertOrder({ ...order, quantity: remainingQty });
     }
   }
 
+  /**
+   * Given a matched order, remove it from the appropriate orderbook if
+   * it is found
+   */
+  removeOrder(order) {
+    pass;
+  }
 
+  /**
+   * Entry point for matching, calls the proper function based on limit or market order
+   */
   matchOrder(newOrder) {
     this.resortOrders();
     if (newOrder.type === "MARKET") {
       return this.matchMarketOrder(newOrder);
     } else {
-      const { matched } = this.findMatches(newOrder);
+      const matched = this.findMatches(newOrder);
       this.handlePartialOrder(newOrder, remainingQty);
       return matched;
     }
   }
 
+  /**
+   * Send matched, cancelled, expired orders to order execution service
+   */
   flushOrders() {
-    // send matched, cancelled, expired orders to order execution service
     console.log("Flushing orders");
     console.log("Matched orders:", this.matchedOrders.length);
     this.sendOrdersToOrderExecutionService(
@@ -129,17 +166,20 @@ module.exports = class OrderBook {
     );
   }
 
+  /**
+   * Insert a new order into the orderbook and resort
+   */
   insertOrder(order) {
-    if (order.order_type === "LIMIT") {
-      if (order.is_buy) {
-        this.buyOrders.push(order);
-      } else {
-        this.sellOrders.push(order);
-      }
-      this.resort();
+    // if (order.order_type === "LIMIT") {
+    if (order.is_buy) {
+      this.buyOrders.push(order);
     } else {
-      this.matchMarketOrder(order);
+      this.sellOrders.push(order);
     }
+    this.resort();
+    // } else {
+    //   this.matchMarketOrder(order);
+    // }
   }
 
   async sendOrdersToOrderExecutionService(

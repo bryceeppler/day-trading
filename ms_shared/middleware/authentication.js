cef365f12c43c21143e9ba270a799b6800357f5c
Freya Engman <f-engman@hotmail.com>
2024-02-19 08:54:06 -0800

    fix error handling

--- ms_shared/middleware/authentication.js
+++ ms_shared/middleware/authentication.js
@@ -1,16 +1,16 @@
 const jwt = require('jsonwebtoken');
-const { STATUS_CODE } = require('../lib/enums');
 const { handleError, createError } = require('../lib/apiHandling');
+const { STATUS_CODE } = require('../lib/enums');
 
 exports.authenticateToken = (accessToken) => async (req, res, next) =>
 {
   const authHeader = req.headers['authorization'];
   const token = authHeader?.split(' ')?.[1];
-  if (!token) handleError(createError("Unauthorized", STATUS_CODE.UNAUTHORIZED), res, next);
+  if (!token) return handleError(createError("Unauthorized", STATUS_CODE.UNAUTHORIZED), res, next);
 
   jwt.verify(token, accessToken, (error, user) =>
   {
-    if (error) handleError(createError("Unauthorized", STATUS_CODE.UNAUTHORIZED), res, next);
+    if (error) return handleError(createError("Unauthorized", STATUS_CODE.UNAUTHORIZED), res, next);
     req.user = user;
     req.token = token;
     next();

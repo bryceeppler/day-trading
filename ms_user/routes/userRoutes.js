d1ff11275341b1c83d84305c83b5cf07e956a86e
M Shaafi Jahangir <mshaafijahangir@gmail.com>
2024-02-19 22:14:13 -0800

    added /getStockPortfolio and /getStockTransactions

--- ms_user/routes/userRoutes.js
+++ ms_user/routes/userRoutes.js
@@ -1,32 +1,35 @@
 const User = require('../models/User');
+const StockTransaction = require('../models/stockTransactionModel');
+const stockTransactionController = require('../controllers/stockTransactionController');
+const WalletTransaction = require('../models/walletTransactionModel');
+const walletTransactionController = require('../controllers/walletTransactionController');
+
 const { authenticateToken } = require('../middleware/authenticateToken');
 
 const express = require('express');
 const router = express.Router();
 
-// getStockPrices
-// async function getStockPrices(req, res) {
-//     try {
-//         // Assuming req.user is populated by the authenticateToken middleware
-//         const user = await User.findById(req.user.userId);
-//         if (!user) {
-//             return res.status(404).json({ success: false, message: "User not found" });
-//         }
-        
-//         // Example data to be returned
-//         const stocks = [
-//             { stock_id: 1, stock_name: "Apple", current_price: 100 }, //initialize them
-//             { stock_id: 2, stock_name: "Google", current_price: 200 }
-//         ];
+// getStockPortfolio
+async function getStockPortfolio(req, res) {
+  try {
+      // Assuming req.user is populated by the authenticateToken middleware
+      const user = await User.findById(req.user.userId);
+      if (!user) {
+          return res.status(404).json({ success: false, message: "User not found" });
+      }
+      
+      // Fetch user's actual stock portfolio from the database
+      const stocks = await StockTransaction.find({ user: user._id });
 
-//         return res.status(200).json({ success: true, data: stocks });
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json({ success: false, message: 'Server error' });
-//     }
-// }
+      // Return the user's stock portfolio
+      return res.status(200).json({ success: true, data: stocks });
+  } catch (error) {
+      console.error(error);
+      return res.status(500).json({ success: false, message: 'Server error' });
+  }
+}
 
-//getWalletBalance
+// getWalletBalance
 async function getWalletBalance(req, res) {
     try {
       // Assuming req.user is populated by the authenticateToken middleware
@@ -43,8 +46,42 @@ async function getWalletBalance(req, res) {
     }
 }
 
-// Define the route and use the authenticateToken middleware
-// router.get('/getStockPrices', authenticateToken, getStockPrices);
+// getStockTransactions
+async function getStockTransactions(req, res, next) {
+  try {
+      // Assuming req.user is populated by the authenticateToken middleware
+      const user = await User.findById(req.user.userId);
+      if (!user) {
+          return res.status(404).json({ success: false, message: "User not found" });
+      }
+
+      // Fetch user's actual stock transactions from the database
+      const stockTransactions = await StockTransaction.find({ user: user._id, is_deleted: false }).sort({ time_stamp: 1 });
+
+      // Transform stock transactions
+      const transformedStockTransactions = stockTransactions.map(tx => ({
+          stock_tx_id: tx._id,
+          stock_id: tx.stock_id,
+          wallet_tx_id: tx.wallet_tx_id,
+          order_status: tx.order_status,
+          is_buy: tx.is_buy,
+          order_type: tx.order_type,
+          stock_price: tx.stock_price,
+          quantity: tx.quantity,
+          time_stamp: tx.time_stamp,
+      }));
+
+      // Return the transformed data
+      return res.status(200).json({ success: true, data: transformedStockTransactions });
+  } catch (error) {
+      console.error('Error getting stock transactions:', error);
+      return res.status(500).json({ success: false, message: `Internal Server Error: ${error.message}` });
+  }
+}
+
+// Define the routes and use the authenticateToken middleware
+router.get('/getStockPortfolio', authenticateToken, getStockPortfolio);
 router.get('/getWalletBalance', authenticateToken, getWalletBalance);
+router.get('/getStockTransactions', authenticateToken, getStockTransactions);
 
 module.exports = router;

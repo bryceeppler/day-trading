3f68f687d58a6a1d9376841b20c64155b48dd548
Freya Engman <f-engman@hotmail.com>
2024-02-20 16:10:23 -0800

    update return method in authenticaiton

--- ms_shared/middleware/authentication.js
+++ ms_shared/middleware/authentication.js
@@ -1,16 +1,15 @@
 const jwt = require('jsonwebtoken');
-const { handleError, createError } = require('../lib/apiHandling');
-const { STATUS_CODE } = require('../lib/enums');
+const { errorReturn } = require('../lib/apiHandling');
 
 exports.authenticateToken = (accessToken) => async (req, res, next) =>
 {
   const authHeader = req.headers['authorization'];
   const token = authHeader?.split(' ')?.[1];
-  if (!token) return handleError(createError("Unauthorized", STATUS_CODE.OK), res, next);
+  if (!token) return errorReturn(res, "Unauthorized");
 
   jwt.verify(token, accessToken, (error, user) =>
   {
-    if (error) return handleError(createError("Unauthorized", STATUS_CODE.OK), res, next);
+    if (error) return errorReturn(res, "Unauthorized");
     req.user = user;
     req.token = token;
     next();

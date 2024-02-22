bfaf53b4fdad48287cb6a43e5e36439e1c6c8463
Bryce Eppler <eppler97@gmail.com>
2024-02-22 04:29:08 -0800

    Order creation fetches current stock price on null price input

--- ms_order_creation/controllers/orders.controller.js
+++ ms_order_creation/controllers/orders.controller.js
@@ -1,30 +1,37 @@
-const { handleError, successReturn, errorReturn } = require('../shared/lib/apiHandling');
-const orderService = require('../services/orders.service')
+const {
+  handleError,
+  successReturn,
+  errorReturn,
+} = require("../shared/lib/apiHandling");
+const orderService = require("../services/orders.service");
 
 exports.placeStockOrder = async (req, res, next) => {
   try {
-		const is_buy = req.body.is_buy;
-		const token = req.token;
-		const orderDetails =  {
-			user_id: req.user?.userId,
-			stock_id: req.body.stock_id,
-			quantity: req.body.quantity,
-			price: req.body.price,
-			order_type: req.body.order_type,
-			is_buy
-		}
+    if (!req.body.price) {
+      const price = await orderService.getCurrentStockPrice(req.body.stock_id);
+      req.body.price = price;
+    }
+    const is_buy = req.body.is_buy;
+    const token = req.token;
+    const orderDetails = {
+      user_id: req.user?.userId,
+      stock_id: req.body.stock_id,
+      quantity: req.body.quantity,
+      price: req.body.price,
+      order_type: req.body.order_type,
+      is_buy,
+    };
 
-		let error;
-		if (is_buy) {
-			error = await orderService.placeOrder(orderDetails, token)
-		} else {
-			error = await orderService.sellOrder(orderDetails, token)
-		}
+    let error;
+    if (is_buy) {
+      error = await orderService.placeOrder(orderDetails, token);
+    } else {
+      error = await orderService.sellOrder(orderDetails, token);
+    }
 
-		
-		if (error) {
-			return errorReturn(res, error)
-		}
+    if (error) {
+      return errorReturn(res, error);
+    }
     successReturn(res);
   } catch (error) {
     handleError(error, res, next);
@@ -33,12 +40,12 @@ exports.placeStockOrder = async (req, res, next) => {
 
 exports.cancelStockTransaction = async (req, res, next) => {
   try {
-		const params =  {
-			stock_tx_id: req.body.stock_tx_id
-		}
+    const params = {
+      stock_tx_id: req.body.stock_tx_id,
+    };
 
-		await orderService.cancelStockTransaction(params, req.token)
-		  
+    await orderService.cancelStockTransaction(params, req.token);
+
     successReturn(res);
   } catch (error) {
     handleError(error, res, next);

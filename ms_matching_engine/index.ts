0a2d96e39e8ad21cea41626a2af875b30d1933b3
Bryce Eppler <eppler97@gmail.com>
2024-02-23 12:04:33 -0800

    changing expiry polling

--- ms_matching_engine/index.ts
+++ ms_matching_engine/index.ts
@@ -23,7 +23,22 @@ app.listen(port, () => {
   console.log(`Matching engine microservice on port ${port}...`);
 });
 
-setInterval(() => {
-  orderBook.checkForExpiredOrders();
-  orderBook.flushOrders();
-}, 10000); // every 10 sec for now
+// setInterval(() => {
+//   orderBook.checkForExpiredOrders();
+//   orderBook.flushOrders();
+// }, 10000); // every 10 sec for now
+
+async function constantExpiryPoll() {
+  while (true) {
+    try {
+      orderBook.checkForExpiredOrders();
+      orderBook.flushOrders();
+      await new Promise(resolve => setTimeout(resolve, 200)); // 200ms (5 times per second)
+    } catch (error) {
+      console.error('Error during order polling: ', error);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+  }
+}
+
+constantExpiryPoll();

49b48cba825d268b9d3bb7daabef10455cc8de5e
Freya Engman <f-engman@hotmail.com>
2024-03-04 09:49:13 -0800

    fixes in execution service

--- ms_shared/config/rabbitmq.js
+++ ms_shared/config/rabbitmq.js
@@ -3,32 +3,36 @@ const amqp = require('amqplib/callback_api');
 let rabbitConnection;
 let rabbitChannel;
 
-exports.connectToRabbitMQ = (rabbitMqUrl) =>
+exports.connectToRabbitMQ = async (rabbitMqUrl) =>
 {
-    amqp.connect(rabbitMqUrl, function (err, connection)
-    {
-        if (err)
-        {
-            console.error('Error connecting to RabbitMQ:', err);
-            return;
-        }
-
-        console.log('Connected to RabbitMQ successfully.');
-
-        connection.createChannel(function (err, channel)
+    return new Promise((resolve, reject) => {
+        amqp.connect(rabbitMqUrl, function (err, connection)
         {
             if (err)
             {
-                console.error('Error creating channel:', err);
+                console.error('Error connecting to RabbitMQ:', err);
+                reject(err);
                 return;
             }
 
-            console.log('Channel created successfully.');
-            rabbitConnection = connection;
-            rabbitChannel = channel;
-            return channel;
-        });
-    })
+            console.log('Connected to RabbitMQ successfully.');
+
+            connection.createChannel(function (err, channel)
+            {
+                if (err)
+                {
+                    console.error('Error creating channel:', err);
+                    reject(err);
+                    return;
+                }
+
+                console.log('Channel created successfully.');
+                rabbitConnection = connection;
+                rabbitChannel = channel;
+                resolve({ connection, channel });
+            });
+        })
+    });
 };
 
 // Function to publish message to RabbitMQ queue
@@ -47,34 +51,3 @@ exports.publishToQueue = async (queueName, message) =>
         throw new Error('Error publishing message to queue: ' + error.message);
     }
 }
-// Function to publish message to RabbitMQ queue
-exports.consumeFromQueue = async (queueName, handleMessage) =>
-{
-    try
-    {
-        if (!rabbitChannel) {
-            throw new Error('RabbitMQ channel is null');
-        }
-        await rabbitChannel.assertQueue(queueName, { durable: true });
-        rabbitChannel.consume(
-            queueName,
-            async (data) =>
-            {
-              if (data)
-              {
-                const message = JSON.parse(data.content.toString());
-                console.log("Recieved Execute Order Message: ", messeage);
-                handleMessage(message);
-              }
-            },
-            { noAck: true }
-          );
-        console.log('Message published to queue:', message);
-    } catch (error)
-    {
-        throw new Error('Error subscribing to queue: ' + error.message);
-    }
-}
-
-// Export rabbitChannel
-exports.rabbitChannel = rabbitChannel;

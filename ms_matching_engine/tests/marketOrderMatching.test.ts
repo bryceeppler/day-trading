beb4d49c65205c44f2e326d57fb52a47c5c6b482
Bryce Eppler <eppler97@gmail.com>
2024-03-05 05:42:43 -0800

    adding unit test for freya's scenario

--- ms_matching_engine/tests/marketOrderMatching.test.ts
+++ ms_matching_engine/tests/marketOrderMatching.test.ts
@@ -9,6 +9,74 @@ describe("OrderBook Market Order Tests", () => {
     orderbook = new OrderBook(StockTransaction);
   });
 
+  // Preconditions:
+
+  // User 1 has 500 quantity of Stock A
+
+  // User 2 has sufficient funds in their account.
+
+  // Steps to Reproduce:
+
+  it("executes with correct price", () => {
+    // This tests a scenario described by Freya where the incorrect order is matched.
+    // We have no knowledge wallet transactions or anything, we just check the correct
+    // message is sent to the execution service.
+
+    // User 1 sell limit order Stock A quantity 10 at price 150
+    const order1: OrderBookOrder = {
+      user_id: "1",
+      stock_tx_id: "1",
+      stock_id: "1",
+      quantity: 10,
+      is_buy: false,
+      order_type: OrderType.LIMIT,
+      timestamp: new Date(new Date().getTime() - 5000),
+      executed: false,
+      price: 150,
+    };
+
+    orderbook.matchOrder(order1);
+
+    // User 1 sell limit order Stock A quantity 10 at price 125
+    const order2: OrderBookOrder = {
+      user_id: "1",
+      stock_tx_id: "1",
+      stock_id: "1",
+      quantity: 10,
+      is_buy: false,
+      order_type: OrderType.LIMIT,
+      timestamp: new Date(),
+      executed: false,
+      price: 125,
+    };
+
+    orderbook.matchOrder(order2);
+
+    // User 2 buy market order Stock A quantity 5
+    const order3: OrderBookOrder = {
+      user_id: "2",
+      stock_tx_id: "1",
+      stock_id: "1",
+      quantity: 5,
+      is_buy: true,
+      order_type: OrderType.MARKET,
+      timestamp: new Date(),
+      executed: false,
+      price: 150,
+    };
+
+    // User 2 Stock transaction shows buy order complete at price 150 and wallet transaction of 750
+    // it should buy at the best available price
+    const [matched, remainingQuantity] = orderbook.matchOrder(order3);
+    console.log(matched);
+    expect(matched.length).toBe(1);
+    expect(matched[0].quantity).toBe(5);
+    expect(remainingQuantity).toBe(0);
+    expect(orderbook.buyOrders.length).toBe(0);
+    expect(orderbook.sellOrders.length).toBe(2);
+    expect(matched[0].matchPrice).toBe(125);
+  });
+
   it("execute a sell market order at the best available buy price", () => {
     const olderTimeStamp = new Date(new Date().getTime() - 1000);
     const newerTimeStamp = new Date();

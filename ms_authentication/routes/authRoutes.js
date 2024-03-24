97ba7c938a7523dd70828aca75152ae20d575ae1
M Shaafi Jahangir <mshaafijahangir@gmail.com>
2024-03-23 23:19:33 -0700

    removed register endpoint from ms_user and unnecessary packages

--- ms_authentication/routes/authRoutes.js
+++ ms_authentication/routes/authRoutes.js
@@ -26,31 +26,6 @@ const validateName = (name) =>
   return name.trim().length > 0 && nameRegex.test(name);
 };
 
-// // Login route
-// router.post('/login', async (req, res) =>
-// {
-//   try
-//   {
-//     const { user_name, password } = req.body;
-//     const userName = user_name.trim();
-//     let user = await User.findOne({  user_name: userName });
-//     if (!user) {
-//       return res.status(200).json({ success: false, data: {error: 'User does not exist'}});
-//     }
-//     // Compare the plaintext password with the hashed password stored in the database
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) {
-//       return res.status(200).json({ success: false, data: {error: 'Invalid Credentials'}});
-//     }
-
-//     const token = jwt.sign({ userId: user._id, user_name: user.user_name, name: user.name }, process.env.JWT_SECRET, { expiresIn: '1h' });
-//     res.status(200).json({ success: true, data: { token } });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ success: false, data: {error: 'Server error' }});
-//   }
-// });
-
 // Register route
 router.post('/register', async (req, res) =>
 {

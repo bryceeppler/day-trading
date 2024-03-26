a5dbf72d292354e70e28a70dc10a8291b7d02836
Freya Engman <f-engman@hotmail.com>
2024-03-25 17:16:20 -0700

    fix controller

--- ms_authentication/controllers/authController.js
+++ ms_authentication/controllers/authController.js
@@ -3,54 +3,59 @@ const {
     handleError,
     successReturn,
     errorReturn,
-  } = require("../shared/lib/apiHandling");
+} = require("../shared/lib/apiHandling");
 const authService = require('../services/authService')
 
-exports.register = async (req, res, next) => {
-    try
-  {
-    console.log("Registering ---------------------------------")
-    const registerDetails = 
-    { 
-        user_name: req.body.user_name, 
-        password: req.body.password, 
-        name: req.body.name };
-
-    console.log(registerDetails);
-    
-    const error = authService.register(registerDetails);
-    
-    if (!error) {
-        return errorReturn(res, error);
-    }
-
-    successReturn(res);
-
-  } catch (error) {
-    return handleError(error, res, next);
-  }
-};
-
-exports.login = async (req, res, next) => {
+exports.register = async (req, res, next) =>
+{
     try
     {
-      const userDetails = 
-        { 
-            user_name: req.body.user_name, 
-            password: req.body.password 
+        console.log("Registering ---------------------------------")
+        const registerDetails =
+        {
+            user_name: req.body.user_name,
+            password: req.body.password,
+            name: req.body.name
         };
 
-    const token = authService.login(userDetails);
-    
-    if (!token) {
-        return errorReturn(res, error);
+        console.log(registerDetails);
+
+        const error = await authService.register(registerDetails);
+
+        if (!error)
+        {
+            return errorReturn(res, error);
+        }
+
+        successReturn(res);
+
+    } catch (error)
+    {
+        return handleError(error, res, next);
     }
+};
 
-    successReturn(res, token);
+exports.login = async (req, res, next) =>
+{
+    try
+    {
+        const userDetails =
+        {
+            user_name: req.body.user_name,
+            password: req.body.password
+        };
 
-     
-  } catch (error) {
-    return handleError(error, res, next);
-  }
+        const token = await authService.login(userDetails);
+        if (!token)
+        {
+            return errorReturn(res, error);
+        }
+
+        successReturn(res, token);
+
+    } catch (error)
+    {
+        return handleError(error, res, next);
+    }
 };
 
--- ms_authentication/services/authService.js
+++ ms_authentication/services/authService.js
@@ -8,6 +8,7 @@ const {
 } = require("../shared/lib/apiHandling");
 const redis = require("../shared/config/redis");
 const User = require('../shared/models/userModel');
+const { STATUS_CODE } = require("../shared/lib/enums");
 
 
 exports.login = async (data) =>
@@ -17,16 +18,21 @@ exports.login = async (data) =>
         let user = await redis.fetchByUserName(data.user_name);
         if (!user)
         {
-            return errorReturn('User does not exist');
+            return createError('User does not exist', STATUS_CODE.OK);
         }
         // Compare the plaintext password with the hashed password stored in the database
         const isMatch = await bcrypt.compare(data.password, user.password);
         if (!isMatch)
         {
-            return errorReturn('Invalid Credentials');
+            return createError('Invalid Credentials', STATUS_CODE.OK);
         }
 
-        return jwt.sign({ userId: user._id, user_name: user.user_name, name: user.name }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ userId: user._id, user_name: user.user_name, name: user.name }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        if (!token)
+        {
+            return createError('error creating token');
+        }
+        return token;
     } catch (error)
     {
         console.error(error);
@@ -42,13 +48,14 @@ exports.register = async (data) =>
 
         if (existingUser)
         {
-            return errorReturn('User already exists');
+            console.log('user already exists', existingUser.user_name);
+            return createError('User already exists', STATUS_CODE.OK);
         }
 
         const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS);
         let hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
-        const newUser = new User({ user_name: data.user_name, password: hashedPassword, name: data.name});
+        const newUser = new User({ user_name: data.user_name, password: hashedPassword, name: data.name });
         await redis.createUser(newUser)
 
     } catch (error)
